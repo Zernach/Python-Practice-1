162bcd397a517e374e0fd79ababd93bcdf811aa4
Zernach <ryan@zernach.com>
2020-06-03 23:22:23 -0400

    module 4

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,8 +11,7 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
-
 # Use the 'format' string method to print the same thing
-
 # Finally, print the same thing using an f-string
+
+print(f'x = {x} \ny = {y} \nz = {z}')
